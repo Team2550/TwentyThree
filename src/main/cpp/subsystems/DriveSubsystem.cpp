8686d86074306f4e68fbb2c08ea68af968a4f1d3
Garin Hamburg <76193294+GarinHamburg@users.noreply.github.com>
2023-01-19 17:32:14 -0800

    DriveSubsystem changes
    * Replaced kLeftMotorPort & kRightMotorPort with kLeftMotorPorts[] and kRightMotorPorts[]
    * Set leftSpeed to be inverted
    * Made m_left and m_right motorControllerGroups of m_front and m_rear, left and right respectively
    * The code should now calibrate an ADIS16470 IMU

--- src/main/cpp/subsystems/DriveSubsystem.cpp
+++ src/main/cpp/subsystems/DriveSubsystem.cpp
@@ -3,8 +3,10 @@
 using namespace DriveConstants;
 
 DriveSubsystem::DriveSubsystem()
-	: m_left{kLeftMotorPort},
-	  m_right{kRightMotorPort},
+	: m_frontLeft{kLeftMotorPorts[0]},
+	  m_rearLeft{kLeftMotorPorts[1]},
+	  m_frontRight{kRightMotorPorts[0]},
+	  m_rearRight{kRightMotorPorts[1]},
 	  m_leftEncoder{kLeftEncoderPorts[0], kLeftEncoderPorts[1], frc::Encoder::EncodingType::k2X},
 	  m_rightEncoder{kRightEncoderPorts[0], kRightEncoderPorts[1], frc::Encoder::EncodingType::k2X} {
 	// Set the distance per pulse for the encoders
@@ -12,7 +14,7 @@ DriveSubsystem::DriveSubsystem()
 	m_rightEncoder.SetDistancePerPulse(kEncoderDistancePerPulse);
 }
 
-void DriveSubsystem::TankDrive(double leftSpeed, double rightSpeed) { m_drive.TankDrive(leftSpeed, rightSpeed); }
+void DriveSubsystem::TankDrive(double leftSpeed, double rightSpeed) { m_drive.TankDrive(-leftSpeed, rightSpeed); }
 
 void DriveSubsystem::ResetEncoders() {
 	m_leftEncoder.Reset();
