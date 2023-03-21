ba8dd5b1c0249d12216bcb5469b0d3a0a5a3e17f
Garin Hamburg <garin.w.hamburg@gmail.com>
2023-03-20 18:14:31 -0700

    Lables & Turbo winch

--- src/main/cpp/sequences/Drive.cpp
+++ src/main/cpp/sequences/Drive.cpp
@@ -8,6 +8,11 @@ Drive::Drive(DriveSubsystem* subsystem)
 void Drive::Initialize() { m_speedMult = 0.65; }
 
 void Drive::Execute() {
+	// Smartdashboard outputs for controls
+	frc::SmartDashboard::PutNumber("Forward speed:", -m_driverController.GetLeftY());
+	frc::SmartDashboard::PutNumber("Turning speed:", -m_driverController.GetRightX());
+
+	// Movement + Turbo toggle
 	if (m_driverController.GetRightBumper() == 1) {
 		frc::SmartDashboard::PutString("Turbo status:", "✅");
 		m_drive->ArcadeDrive(((m_driverController.GetLeftY())), ((-m_driverController.GetRightX())));
@@ -17,31 +22,37 @@ void Drive::Execute() {
 			((m_driverController.GetLeftY()) * m_speedMult), ((-m_driverController.GetRightX()) * m_speedMult));
 	}
 
-	frc::SmartDashboard::PutNumber("Forward speed:", -m_driverController.GetLeftY());
-	frc::SmartDashboard::PutNumber("Turning speed:", -m_driverController.GetRightX());
-
+	// Arm movement
 	if (m_armController.GetLeftTriggerAxis() < m_armController.GetRightTriggerAxis()) {
 		m_drive->MoveArm(-m_armController.GetRightTriggerAxis());
 		frc::SmartDashboard::PutString("Arm status:", "MOVING UP");
 	} else {
 		if (m_armController.GetLeftTriggerAxis() > m_armController.GetRightTriggerAxis()) {
 			m_drive->MoveArm(m_armController.GetLeftTriggerAxis());
+			m_drive->DriveWinch(-0.2);
 			frc::SmartDashboard::PutString("Arm status:", "MOVING DOWN");
 		};
 	}
 	if (m_armController.GetLeftTriggerAxis() == m_armController.GetRightTriggerAxis()) {
 		m_drive->MoveArm(0.0);
+		m_drive->DriveWinch(0);
 		frc::SmartDashboard::PutString("Arm status:", "NOT MOVING");
 	}
 
-	if (m_driverController.GetBButton() == 1) {
-		m_drive->DriveWinch(-0.25);
-	} else if (m_driverController.GetAButton() == 1) {
+	// Winch when button
+	if (m_driverController.GetAButton() == 1) {
 		m_drive->DriveWinch(-0.5);
-	} else {
+	} else if (m_driverController.GetBButton() == 1) {
+		m_drive->DriveWinch(-1.0);
+	}
+
+	// Clear winch if no input & arm is not moving down.
+	if (m_driverController.GetAButton() == 0 && m_driverController.GetBButton() == 0
+		&& (m_armController.GetLeftTriggerAxis() < m_armController.GetRightTriggerAxis())) {
 		m_drive->DriveWinch(0);
 	}
 
+	// This is labled ArmTask, but it really is HandTask
 	if (m_armController.GetAButton() == 1 && m_armController.GetBButton() == 0) {
 		m_drive->ArmRelease();
 	}
